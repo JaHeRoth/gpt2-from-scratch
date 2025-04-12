f713f518eb25ecf7442471349b411421f15723fa
Jacob H. Rothschild <jacob.r.developer@gmail.com>
2025-04-12 16:25:52 +0200

    Dataset and DataLoader exploration

--- playground.py
+++ playground.py
@@ -1,4 +1,6 @@
 # %%
+import random
+
 import torch
 from torch import nn
 from tqdm import tqdm
@@ -56,6 +58,45 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
+
+# %%
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(cols * rows):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i + 1)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+
+# %%
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+# %%
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+print(f"Label: {label}")
 
 
 # %%
