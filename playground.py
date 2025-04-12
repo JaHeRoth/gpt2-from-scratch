bd9be5d34f095a06767518c5181cdd42783a605a
Jacob H. Rothschild <jacob.r.developer@gmail.com>
2025-04-12 16:09:58 +0200

    Torchvision

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 # %%
 import torch
+from torch import nn
 from tqdm import tqdm
 
 data = [[1, 2], [3, 5], [5, 8], [7, 11]]
@@ -14,6 +15,7 @@ z = x_w_ones @ weights
 print(z)
 
 # %%
+# Training with manual weight updates
 lr = 0.01
 y = torch.tensor([[4], [11], [18], [25]]).float()
 
@@ -32,5 +34,43 @@ for i in tqdm(range(10_000)):
         print(w)
         print(x_w_ones @ w)
         print("-" * 80)
+
+# %%
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+
+# %%
+class Linear(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int):
+        super().__init__()
+        self.w = torch.rand(in_dim, out_dim, requires_grad=True)
+        self.b = torch.ones(out_dim, 1, requires_grad=True)
+
+    def forward(self, x):
+        return x @ self.w + self.b
+
+
+# %%
+# Training with optimizer
+
 
 # %%
