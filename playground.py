79cf08955388c94c4517e1b58bccf38bf0dd4cfe
Jacob H. Rothschild <jacob.rothschild@quantco.com>
2025-04-14 09:21:51 +0200

    Exploration

--- playground.py
+++ playground.py
@@ -1,9 +1,12 @@
 # %%
 import random
+from typing import Callable
 
 import torch
 from torch import nn
+from torch._C.cpp.nn import Module
 from tqdm import tqdm
+from torch.optim import Optimizer
 
 data = [[1, 2], [3, 5], [5, 8], [7, 11]]
 x = torch.tensor(data)
@@ -220,20 +223,189 @@ z = x @ w + b
 print(z.requires_grad)
 print(z.detach().requires_grad)
 
+# %%
+def train_loop(dataloader: DataLoader, model: nn.Module, loss_fn: Callable, optimizer: Optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch_i, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch_i % 100 == 0:
+            loss, current = loss.item(), batch_i * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader: DataLoader, model: nn.Module, loss_fn: Callable):
+    num_batches = len(dataloader)
+    test_loss, accuracy = 0, 0
+    model.eval()
+    with torch.no_grad():
+        for batch_i, (X, y) in enumerate(dataloader):
+            pred: torch.Tensor = model(X)
+            test_loss += loss_fn(pred, y).item() / num_batches
+            accuracy += (pred.argmax(1) == y).type(torch.float).mean().item() / num_batches
+    print(f"Test Error: \n Accuracy: {(100 * accuracy):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+model = NeuralNetwork()
+
+learning_rate = 1e-3
+batch_size = 64
+epochs = 10
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+for t in range(epochs):
+    print(f"Epoch {t + 1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
 
 # %%
-class Linear(nn.Module):
-    def __init__(self, in_dim: int, out_dim: int):
-        super().__init__()
-        self.w = torch.rand(in_dim, out_dim)
-        self.b = torch.ones(out_dim, 1)
+import torch
+import torchvision.models as models
 
-    def forward(self, x):
-        return x @ self.w + self.b
-
+model = models.vgg16(weights="IMAGENET1K_V1")
+torch.save(model.state_dict(), "model_weights.pth")
 
 # %%
-# Training with optimizer
+model = models.vgg16()
+model.load_state_dict(torch.load('model_weights.pth', weights_only=True))
+model.eval()
+print(model)
 
+# %%
+torch.save(model, "model.pth")
+model = torch.load("model.pth", weights_only=False)
+print(model)
+
+# %%
+from pathlib import Path
+import requests
+
+DATA_PATH = Path("data")
+PATH = DATA_PATH / "mnist"
+
+PATH.mkdir(parents=True, exist_ok=True)
+
+URL = "https://github.com/pytorch/tutorials/raw/main/_static/"
+FILENAME = "mnist.pkl.gz"
+
+if not (FILE_PATH := PATH / FILENAME).exists():
+    with open(FILE_PATH, "wb") as f:
+        f.write(
+            requests.get(URL + FILENAME).content
+        )
+
+# %%
+import pickle
+import gzip
+
+with gzip.open(FILE_PATH, "rb") as f:
+    mnist = pickle.load(f, encoding="latin-1")
+    x_train, y_train = mnist[0]
+    x_valid, y_valid = mnist[1]
+
+# %%
+%matplotlib inline
+
+# %%
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.imshow(x_train[0].reshape(28, 28), cmap="gray")
+plt.show()
+
+# %%
+import torch
+
+x_train, y_train, x_valid, y_valid = (
+    torch.tensor(data)
+    for data in (x_train, y_train, x_valid, y_valid)
+)
+n, c = x_train.shape
+
+print(x_train, y_train)
+print(x_train.shape)
+print(y_train.min(), y_train.max())
+
+# %%
+import math
+
+weights = torch.randn(784, 10) / math.sqrt(784)
+weights.requires_grad_()
+bias = torch.zeros(10, requires_grad=True)
+
+# %%
+def log_softmax(x: torch.Tensor):
+    return x - x.exp().sum(-1).log().unsqueeze(-1)
+
+def model(x: torch.Tensor):
+    return log_softmax(x @ weights + bias)
+
+# %%
+bs = 64
+
+x_batch = x_train[:bs]
+preds = model(x_batch)
+print(preds[0], preds.shape)
+
+# %%
+def nll(input, target):
+    return -input[range(target.shape[0]), target].mean()
+
+loss_func = nll
+
+# %%
+y_batch = y_train[:bs]
+print(loss_func(preds, y_batch))
+
+# %%
+def accuracy(out: torch.Tensor, y: torch.Tensor):
+    preds = out.argmax(dim=1)
+    return (preds == y).float().mean().item()
+
+print(accuracy(preds, y_batch))
+
+# %%
+lr = 0.5
+epochs = 2
+
+for epoch in range(epochs):
+    for i in range(math.ceil(n / bs)):
+        # breakpoint()
+        start_i = i * bs
+        end_i = start_i + bs
+        xb = x_train[start_i:end_i]
+        yb = y_train[start_i:end_i]
+        pred = model(xb)
+        loss = loss_func(pred, yb)
+
+        loss.backward()
+        with torch.no_grad():
+            weights -= lr * weights.grad
+            bias -= lr * bias.grad
+            weights.grad.zero_()
+            bias.grad.zero_()
+
+# %%
+print(loss_func(model(xb), yb), accuracy(model(xb), yb))
+
+# %%
+import torch.nn.functional as F
+
+loss_func = F.cross_entropy
+
+def model(xb):
+    return xb @ weights + bias
+
+print(loss_func(model(xb), yb), accuracy(model(xb), yb))
 
 # %%
