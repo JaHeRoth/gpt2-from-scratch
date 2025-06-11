2aca381f1167fb9f5dcaa8ea2ec956dccf6f1c74
Jacob H. Rothschild <jacob.rothschild@quantco.com>
2025-06-11 09:12:25 +0200

    Moved data loading and preprocessing into data handler file

---
+++ data_handler.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import torch
+from datasets import load_dataset, load_from_disk
+
+
+def tokenize(batch, tokenizer, context_length: int) -> dict[str, list[torch.Tensor]]:
+    # TODO: Sequence packing
+    outputs = tokenizer(
+        batch["text"],
+        truncation=True,
+        max_length=context_length,
+        return_overflowing_tokens=True,
+        return_length=True,
+    )
+    return {
+        "input_ids": [
+            input_ids
+            for length, input_ids in zip(outputs["length"], outputs["input_ids"])
+            if length == context_length
+        ]
+    }
+
+
+def load_preprocessed(hf_path: str, hf_name: str, tokenizer, context_length: int):
+    local_path = Path(f"{hf_path}__{hf_name}__tokenized.hf")  # TODO: Use absolute path
+
+    if local_path.exists():
+        return load_from_disk(str(local_path))
+
+    dataset = load_dataset(hf_path, hf_name)
+    tokenized_ds = dataset.map(
+        lambda batch: tokenize(batch, tokenizer, context_length),
+        batched=True,
+        remove_columns=dataset["train"].column_names,
+    )
+    tokenized_ds.save_to_disk("tokenized-wiki-ds.hf")
+    return tokenized_ds
