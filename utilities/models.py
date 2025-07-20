d7e6d002dca650b75b742d49cfd414fbbe0efee4
Jacob H. Rothschild <jacob.rothschild@quantco.com>
2025-07-20 20:33:53 +0200

    Refactor

--- utilities/models.py
+++ utilities/models.py
@@ -27,36 +27,6 @@ def _build_supporters_for_packed_batch(input_ids: torch.Tensor, eos_token_id: in
     return relative_idx, multihead_additive_attention_mask
 
 
-class PositionalEmbedding(nn.Module):
-    def __init__(self, embedding_dim):
-        super().__init__()
-        self.numerators = 10_000 ** (
-            torch.arange(
-                start=0,
-                end=embedding_dim,
-                step=2,
-            ).float()
-            / embedding_dim
-        )
-
-    def forward(self, input_ids: torch.Tensor):
-        with torch.no_grad():
-            positions = torch.arange(
-                input_ids.shape[1],
-                device=input_ids.device,
-            ).float()
-            numerators = self.numerators.to(input_ids.device)
-            raw_embeddings = positions.unsqueeze(1) @ (1 / numerators).unsqueeze(0)
-            even_embeddings = torch.sin(raw_embeddings)
-            odd_embeddings = torch.cos(raw_embeddings)
-            embeddings = torch.stack(
-                [even_embeddings, odd_embeddings], dim=-1
-            ).view(
-                len(positions), -1
-            )
-            return embeddings.unsqueeze(0).expand(input_ids.shape[0], -1, -1)
-
-
 class Linear(nn.Module):
     def __init__(self, d_in: int, d_out: int, device, bias: bool = True):
         super().__init__()
@@ -134,139 +104,6 @@ class Softmax(nn.Module):
         x = x.float()
         exponentiated = x.exp()
         return exponentiated / exponentiated.sum(dim=dim, keepdim=True)
-
-
-class TransformerEncoderGPT(nn.Module):
-    def __init__(
-        self,
-        d_model: int,
-        nhead: int,
-        num_layers: int,
-        dim_feedforward: int,
-        vocab_size: int,
-        context_length: int,
-        eos_token_id: int,
-        device,
-    ):
-        super().__init__()
-        self.d_model = d_model
-        self.nhead = nhead
-        self.context_length = context_length
-        self.eos_token_id = eos_token_id
-        self.device = device
-        self.token_embedder = nn.Embedding(
-            num_embeddings=vocab_size, embedding_dim=d_model, device=device
-        )
-        self.positional_embedder = nn.Embedding(
-            num_embeddings=context_length, embedding_dim=d_model, device=device
-        )
-        self.transformer = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(
-                d_model=d_model,
-                nhead=nhead,
-                dim_feedforward=dim_feedforward,
-                activation="gelu",
-                device=device,
-                batch_first=True,
-                norm_first=False,
-            ),
-            num_layers=num_layers,
-        )
-        self.apply(self.init_weights)
-        self.decoder = nn.Linear(d_model, vocab_size, bias=False, device=device)
-        self.decoder.weight = self.token_embedder.weight
-
-    @staticmethod
-    def init_weights(m):
-        if isinstance(m, (nn.Linear, nn.Embedding)):
-            nn.init.normal_(m.weight, mean=0.0, std=0.02)
-            if getattr(m, "bias", None) is not None:
-                nn.init.zeros_(m.bias)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.ones_(m.weight)
-            nn.init.zeros_(m.bias)
-        elif isinstance(m, nn.MultiheadAttention):
-            nn.init.normal_(m.in_proj_weight, mean=0.0, std=0.02)
-            nn.init.zeros_(m.in_proj_bias)
-            # `m.out_proj` is an instance of `nn.Linear`, thus already handled by the first condition
-
-    def forward(self, input_ids: torch.Tensor):
-        input_idx, mask = _build_supporters_for_packed_batch(input_ids, eos_token_id=self.eos_token_id, nhead=self.nhead)
-        embedded = self.token_embedder(input_ids) * sqrt(self.d_model) + self.positional_embedder(input_idx)
-        transformed = self.transformer(
-            embedded,
-            mask=mask,
-        )
-        logits = self.decoder(transformed)
-        return logits
-
-
-class TransformerEncoderGPT2(nn.Module):
-    def __init__(
-        self,
-        d_model: int,
-        nhead: int,
-        num_layers: int,
-        dim_feedforward: int,
-        vocab_size: int,
-        context_length: int,
-        eos_token_id: int,
-        device,
-    ):
-        super().__init__()
-        self.d_model = d_model
-        self.nhead = nhead
-        self.num_layers = num_layers
-        self.context_length = context_length
-        self.eos_token_id = eos_token_id
-        self.device = device
-        self.token_embedder = nn.Embedding(
-            num_embeddings=vocab_size, embedding_dim=d_model, device=device
-        )
-        self.positional_embedder = nn.Embedding(
-            num_embeddings=context_length, embedding_dim=d_model, device=device
-        )
-        self.transformer = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(
-                d_model=d_model,
-                nhead=nhead,
-                dim_feedforward=dim_feedforward,
-                activation="gelu",
-                device=device,
-                batch_first=True,
-                norm_first=True,
-            ),
-            num_layers=num_layers,
-        )
-        self.apply(self.init_weights)
-        self.decoder = nn.Linear(d_model, vocab_size, bias=False, device=device)
-        self.decoder.weight = self.token_embedder.weight
-
-    def init_weights(self, m):
-        if isinstance(m, (nn.Linear, nn.Embedding)):
-            std = 0.02
-            if isinstance(m, nn.Linear) and m.weight.shape[0] == self.d_model:
-                std /= sqrt(self.num_layers)
-            nn.init.normal_(m.weight, mean=0.0, std=std)
-            if getattr(m, "bias", None) is not None:
-                nn.init.zeros_(m.bias)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.ones_(m.weight)
-            nn.init.zeros_(m.bias)
-        elif isinstance(m, nn.MultiheadAttention):
-            nn.init.normal_(m.in_proj_weight, mean=0.0, std=0.02)
-            nn.init.zeros_(m.in_proj_bias)
-            # `m.out_proj` is an instance of `nn.Linear`, thus already handled by the first condition
-
-    def forward(self, input_ids: torch.Tensor):
-        input_idx, mask = _build_supporters_for_packed_batch(input_ids, eos_token_id=self.eos_token_id, nhead=self.nhead)
-        embedded = self.token_embedder(input_ids) * sqrt(self.d_model) + self.positional_embedder(input_idx)
-        transformed = self.transformer(
-            embedded,
-            mask=mask,
-        )
-        logits = self.decoder(transformed)
-        return logits
 
 
 class AttentionHead(nn.Module):
@@ -464,4 +301,171 @@ class ParametersGPT2(nn.Module):
                         x = layer(x)
                 encoded = encoded + x
         logits = self.decoder(encoded)
+        return logits
+
+
+# --------------
+# DEPRECATED
+# --------------
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, embedding_dim):
+        super().__init__()
+        self.numerators = 10_000 ** (
+            torch.arange(
+                start=0,
+                end=embedding_dim,
+                step=2,
+            ).float()
+            / embedding_dim
+        )
+
+    def forward(self, input_ids: torch.Tensor):
+        with torch.no_grad():
+            positions = torch.arange(
+                input_ids.shape[1],
+                device=input_ids.device,
+            ).float()
+            numerators = self.numerators.to(input_ids.device)
+            raw_embeddings = positions.unsqueeze(1) @ (1 / numerators).unsqueeze(0)
+            even_embeddings = torch.sin(raw_embeddings)
+            odd_embeddings = torch.cos(raw_embeddings)
+            embeddings = torch.stack(
+                [even_embeddings, odd_embeddings], dim=-1
+            ).view(
+                len(positions), -1
+            )
+            return embeddings.unsqueeze(0).expand(input_ids.shape[0], -1, -1)
+
+
+class TransformerEncoderGPT(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        nhead: int,
+        num_layers: int,
+        dim_feedforward: int,
+        vocab_size: int,
+        context_length: int,
+        eos_token_id: int,
+        device,
+    ):
+        super().__init__()
+        self.d_model = d_model
+        self.nhead = nhead
+        self.context_length = context_length
+        self.eos_token_id = eos_token_id
+        self.device = device
+        self.token_embedder = nn.Embedding(
+            num_embeddings=vocab_size, embedding_dim=d_model, device=device
+        )
+        self.positional_embedder = nn.Embedding(
+            num_embeddings=context_length, embedding_dim=d_model, device=device
+        )
+        self.transformer = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(
+                d_model=d_model,
+                nhead=nhead,
+                dim_feedforward=dim_feedforward,
+                activation="gelu",
+                device=device,
+                batch_first=True,
+                norm_first=False,
+            ),
+            num_layers=num_layers,
+        )
+        self.apply(self.init_weights)
+        self.decoder = nn.Linear(d_model, vocab_size, bias=False, device=device)
+        self.decoder.weight = self.token_embedder.weight
+
+    @staticmethod
+    def init_weights(m):
+        if isinstance(m, (nn.Linear, nn.Embedding)):
+            nn.init.normal_(m.weight, mean=0.0, std=0.02)
+            if getattr(m, "bias", None) is not None:
+                nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.ones_(m.weight)
+            nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.MultiheadAttention):
+            nn.init.normal_(m.in_proj_weight, mean=0.0, std=0.02)
+            nn.init.zeros_(m.in_proj_bias)
+            # `m.out_proj` is an instance of `nn.Linear`, thus already handled by the first condition
+
+    def forward(self, input_ids: torch.Tensor):
+        input_idx, mask = _build_supporters_for_packed_batch(input_ids, eos_token_id=self.eos_token_id, nhead=self.nhead)
+        embedded = self.token_embedder(input_ids) * sqrt(self.d_model) + self.positional_embedder(input_idx)
+        transformed = self.transformer(
+            embedded,
+            mask=mask,
+        )
+        logits = self.decoder(transformed)
+        return logits
+
+
+class TransformerEncoderGPT2(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        nhead: int,
+        num_layers: int,
+        dim_feedforward: int,
+        vocab_size: int,
+        context_length: int,
+        eos_token_id: int,
+        device,
+    ):
+        super().__init__()
+        self.d_model = d_model
+        self.nhead = nhead
+        self.num_layers = num_layers
+        self.context_length = context_length
+        self.eos_token_id = eos_token_id
+        self.device = device
+        self.token_embedder = nn.Embedding(
+            num_embeddings=vocab_size, embedding_dim=d_model, device=device
+        )
+        self.positional_embedder = nn.Embedding(
+            num_embeddings=context_length, embedding_dim=d_model, device=device
+        )
+        self.transformer = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(
+                d_model=d_model,
+                nhead=nhead,
+                dim_feedforward=dim_feedforward,
+                activation="gelu",
+                device=device,
+                batch_first=True,
+                norm_first=True,
+            ),
+            num_layers=num_layers,
+        )
+        self.apply(self.init_weights)
+        self.decoder = nn.Linear(d_model, vocab_size, bias=False, device=device)
+        self.decoder.weight = self.token_embedder.weight
+
+    def init_weights(self, m):
+        if isinstance(m, (nn.Linear, nn.Embedding)):
+            std = 0.02
+            if isinstance(m, nn.Linear) and m.weight.shape[0] == self.d_model:
+                std /= sqrt(self.num_layers)
+            nn.init.normal_(m.weight, mean=0.0, std=std)
+            if getattr(m, "bias", None) is not None:
+                nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.ones_(m.weight)
+            nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.MultiheadAttention):
+            nn.init.normal_(m.in_proj_weight, mean=0.0, std=0.02)
+            nn.init.zeros_(m.in_proj_bias)
+            # `m.out_proj` is an instance of `nn.Linear`, thus already handled by the first condition
+
+    def forward(self, input_ids: torch.Tensor):
+        input_idx, mask = _build_supporters_for_packed_batch(input_ids, eos_token_id=self.eos_token_id, nhead=self.nhead)
+        embedded = self.token_embedder(input_ids) * sqrt(self.d_model) + self.positional_embedder(input_idx)
+        transformed = self.transformer(
+            embedded,
+            mask=mask,
+        )
+        logits = self.decoder(transformed)
         return logits
