Attempting to fix softmax numerical instability issues

utilities/models.py
@@ -102,8 +102,9 @@ class Dropout(nn.Module):
 class Softmax(nn.Module):
     def forward(self, x: torch.Tensor, dim=-1):
         x = x.float()
-        exponentiated = x.exp()
-        return exponentiated / exponentiated.sum(dim=dim, keepdim=True)
+        shift_x = x - x.max(dim=dim, keepdim=True)
+        shift_exp = shift_x.exp()
+        return shift_exp / shift_exp.sum(dim=dim, keepdim=True)
 
 
 class AttentionHead(nn.Module):