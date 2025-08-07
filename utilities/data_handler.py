5a56ff7ed767047bacaa400ad03d69a06f7dbcf4
Jacob H. Rothschild <jacob.rothschild@quantco.com>
2025-08-07 22:51:18 +0200

    tokenize uses uint16 if possible, thus halving the size of the resulting dataset

--- utilities/data_handler.py
+++ utilities/data_handler.py
@@ -5,8 +5,10 @@ from datasets import load_dataset, load_from_disk
 
 
 def tokenize(batch, tokenizer, context_length: int) -> dict[str, list[torch.Tensor]]:
+    dtype = torch.uint16 if len(tokenizer) < 2 ** 16 else torch.uint32
     # +1 since last token of sequences won't be used as input (since has no next token)
     row_length = context_length + 1
+
     raw_tokenized_batch = tokenizer(batch["text"])
     filtered = [
         input_ids
@@ -19,13 +21,14 @@ def tokenize(batch, tokenizer, context_length: int) -> dict[str, list[torch.Tens
         for token_id in input_ids + [tokenizer.eos_token_id]
     ]
     foldable = flattened + [tokenizer.pad_token_id] * ((-len(flattened)) % row_length)
+
     return {
-        "input_ids": torch.tensor(foldable).view(-1, row_length)
+        "input_ids": torch.tensor(foldable, dtype=dtype).view(-1, row_length)
     }
 
 
 def load_preprocessed(hf_path: str, hf_name: str, tokenizer, context_length: int):
-    dataset = load_dataset(hf_path, hf_name, split="train")
+    dataset = load_dataset(hf_path, hf_name, split="train")#.shard(num_shards=1000, index=0)
     train_val_split = dataset.train_test_split(test_size=0.01)
 
     local_path = Path(f"outputs/{hf_path}__{hf_name}__tokenized.hf")
@@ -34,8 +37,9 @@ def load_preprocessed(hf_path: str, hf_name: str, tokenizer, context_length: int
     else:
         tokenized_ds = train_val_split.map(
             lambda batch: tokenize(batch, tokenizer, context_length),
-            batched=True,
             remove_columns=train_val_split["train"].column_names,
+            load_from_cache_file=False,
+            batched=True,
         )
         tokenized_ds.save_to_disk(local_path)
     return dataset, tokenized_ds
